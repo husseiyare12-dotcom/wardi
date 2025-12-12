9fa3f5ed32b6b1131d8e9382981113b7d1bb0629
husseiyare12@gmail.com <husseiyare12@gmail.com>
2025-12-12 21:08:42 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let num = 0;
+const number = document.getElementById("number");
+const emoji = document.getElementById("emoji");
+
+const emojis = ["😀","🔥","🎉","🤲","✨","💯","🕊️","😊","🥰","🌟"];
+
+document.getElementById("plus").addEventListener("click", () => {
+    num++;
+    updateUI();
+});
+
+document.getElementById("minus").addEventListener("click", () => {
+    if(num > 0) num--;
+    updateUI();
+});
+
+document.getElementById("reset").addEventListener("click", () => {
+    num = 0;
+    updateUI();
+});
+
+function updateUI() {
+    number.textContent = num;
+
+    if (num % 10 === 0 && num !== 0) {
+        let index = (num / 10) - 1;
+        emoji.textContent = emojis[index] || "🎉";
+    } else {
+        emoji.textContent = "";
+    }
+}
